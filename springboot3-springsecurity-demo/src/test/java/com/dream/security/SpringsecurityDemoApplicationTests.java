01ec0f58b1a6bfebee7f254a11c6b3ffec2e7773
grepxn <grepxn@gmail.com>
2024-01-16 12:05:05 +0800

    初始化mybatis plus和spring security组件

---
+++ springboot3-springsecurity-demo/src/test/java/com/dream/security/SpringsecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
